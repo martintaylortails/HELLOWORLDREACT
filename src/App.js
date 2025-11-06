54adfc5cd734e803faa4ef2ff1104c134a550b9b
Martin Taylor <martintaylor.tails@gmail.com>
2025-11-06 21:43:20 +0000

    newby

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Route, Routes } from "react-router-dom"
 import Header from "./components/header"
 import Navbar from "./components/navbar"
+import RecordList from "./components/recordList"
 import Create from "./components/create"
 
 function App() {
