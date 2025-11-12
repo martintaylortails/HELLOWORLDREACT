9bab611fce80c1a6fa74f58ebb5f172cd5409633
Martin Taylor <martintaylor.tails@gmail.com>
2025-11-12 21:54:41 +0000

    gh

--- src/components/recordList.js
+++ src/components/recordList.js
@@ -57,7 +57,7 @@ export default function RecordList() {
 
     useEffect(() => {
         async function getRecords() {
-            const response = await fetch(`${process.env.REACT_APP_TEST}/record`)
+            const response = await fetch(`${process.env.REACT_APP_TEST}/api/record`)
 
             if (!response.ok) {
                 const message = `An error occurred: ${response.statusText}`
@@ -71,7 +71,7 @@ export default function RecordList() {
         }
 
         async function getCleaning() {
-            const response = await fetch(`${process.env.REACT_APP_TEST}/cleaning`)
+            const response = await fetch(`${process.env.REACT_APP_TEST}/api/cleaning`)
 
             if (!response.ok) {
                 const message = `An error occurredddd: ${response.statusText}`
